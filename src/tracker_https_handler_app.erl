0e87768757594ecd148e99f0d1737bd159f336d1
NoahFTW123 <noahjlarsen20@gmail.com>
2024-11-15 15:26:40 -0700

    handler update

--- src/tracker_https_handler_app.erl
+++ src/tracker_https_handler_app.erl
@@ -8,7 +8,9 @@ start(_StartType, _StartArgs) ->
     {'_', [
         %{"/", cowboystatic, {priv_file, db_access, "static/index.html"}},
         {"/", toppage_h, []},
-        {"/register", registration_http_handler, []}
+        {"/register", registration_http_handler, []},
+        {"/update", package_monitor_http_handler, []},
+        {"/track/[...]", tracking_http_handler, []}
         %{"/gfriends",get_friends_h,[]},
         %{"/pfriends",set_friends_h,[]},
         %{"/afriend",add_friend_h,[]}
