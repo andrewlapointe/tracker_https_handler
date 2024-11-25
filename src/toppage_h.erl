d46ff47fbc70a1100600d38c13d0ead230f39ea7
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-11-25 15:52:35 -0700

    index page test

--- src/toppage_h.erl
+++ src/toppage_h.erl
@@ -4,12 +4,12 @@
 % -define(MAIN_LOGIC_PID, main_logic).
 
 init(Req0, Opts) ->
-    Req = cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, "[\"Hello world!\"]", Req0),
-    {ok, Req, Opts}.
+    % Req = cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, "[\"Hello world!\"]", Req0),
+    % {ok, Req, Opts}.
 
 
     
-    % {ok, Data} = file:read_file("/public/pages/index.html"),
-    % Req1 = cowboy_req:set_resp_header(<<"content-type">>, <<"text/html">>, Req0),
-    % {ok, Req2} = cowboy_req:reply(200, #{}, Data, Req1),
-    % {ok, Req2, Opts}.
+    {ok, Data} = file:read_file("/priv/pages/index.html"),
+    Req1 = cowboy_req:set_resp_header(<<"content-type">>, <<"text/html">>, Req0),
+    {ok, Req2} = cowboy_req:reply(200, #{}, Data, Req1),
+    {ok, Req2, Opts}.
