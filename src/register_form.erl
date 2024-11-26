c958f941418855662c9a7c0901897240622550ef
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-11-26 13:46:41 -0700

    changed response data type

--- src/register_form.erl
+++ src/register_form.erl
@@ -3,38 +3,35 @@
 
 
 init(Req0, State) ->
-    %% Ensure the request method is POST
-    case cowboy_req:method(Req0) of
+    Method = cowboy_req:method(Req0),
+    case Method of
         <<"POST">> ->
-            %% Get the Content-Type header
-            Headers = cowboy_req:headers(Req0),
-            ContentType = maps:get(<<"content-type">>, Headers, <<"">>),
-            %% Check if Content-Type is application/x-www-form-urlencoded
-            case ContentType of
-                <<"application/x-www-form-urlencoded">> ->
-                    %% Read the POST body
-                    {ok, Body, Req1} = cowboy_req:read_body(Req0),
-                    %% Parse form data
-                    FormData = cowboy_http:parse_qs(Body),
-                    %% Extract the value associated with 'user_data'
-                    UserData = proplists:get_value(<<"user_data">>, FormData, <<"">>),
-                    %% Process the data
-                    io:format("Received data: ~s~n", [UserData]),
-                    %% Send a response back to the client
-                    ResponseBody = <<"Thank you for your submission!">>,
-                    Req2 = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, ResponseBody, Req1),
-                    {ok, Req2, State};
-                _ ->
-                    %% Unsupported Media Type
-                    Req1 = cowboy_req:reply(
-                        415,
-                        #{<<"content-type">> => <<"text/plain">>},
-                        <<"Unsupported Media Type">>,
-                        Req0
-                    ),
-                    {ok, Req1, State}
-            end;
+            %% Read and parse the URL-encoded form data
+            {Fields, Req1} = cowboy_req:read_urlencoded_body(Req0),
+            %% Extract 'user_data' from the form fields
+            UserData = maps:get(<<"user_data">>, Fields, <<"">>),
+            %% Process the data (e.g., log it)
+            io:format("Received data: ~s~n", [UserData]),
+            %% Prepare the response
+            ResponseBody = io_lib:format(
+                "<html><body><h1>Thank you, you submitted: ~s</h1></body></html>",
+                [UserData]
+            ),
+            %% Send the response
+            Req2 = cowboy_req:reply(
+                200,
+                #{<<"content-type">> => <<"text/html">>},
+                ResponseBody,
+                Req1
+            ),
+            {ok, Req2, State};
         _ ->
-            %% Handle other HTTP methods if necessary
-            {ok, Req0, State}
+            %% Handle other methods
+            Req1 = cowboy_req:reply(
+                405,
+                #{<<"content-type">> => <<"text/plain">>},
+                <<"Method Not Allowed">>,
+                Req0
+            ),
+            {ok, Req1, State}
     end.
