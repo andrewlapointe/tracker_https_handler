91184a8a651e63b218d7de7dd570dda4225d755e
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-11-26 13:42:58 -0700

    changed response data type

--- src/register_form.erl
+++ src/register_form.erl
@@ -6,25 +6,35 @@ init(Req0, State) ->
     %% Ensure the request method is POST
     case cowboy_req:method(Req0) of
         <<"POST">> ->
-            %% Check if content-type is application/json
+            %% Get the Content-Type header
             Headers = cowboy_req:headers(Req0),
-            case maps:get(<<"content-type">>, Headers, <<"">>) of
-                <<"application/json">> ->
+            ContentType = maps:get(<<"content-type">>, Headers, <<"">>),
+            %% Check if Content-Type is application/x-www-form-urlencoded
+            case ContentType of
+                <<"application/x-www-form-urlencoded">> ->
+                    %% Read the POST body
                     {ok, Body, Req1} = cowboy_req:read_body(Req0),
-                    %% Decode JSON
-                    {ok, Json} = jsx:decode(Body, [return_maps]),
-                    UserData = maps:get(<<"user_data">>, Json, <<"">>),
+                    %% Parse form data
+                    FormData = cowboy_http:parse_qs(Body),
+                    %% Extract the value associated with 'user_data'
+                    UserData = proplists:get_value(<<"user_data">>, FormData, <<"">>),
                     %% Process the data
-                    io:format("Received JSON data: ~s~n", [UserData]),
-                    %% Send a response
-                    ResponseBody = <<"Received your JSON data!">>,
-                    {ok, Req2} = cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, ResponseBody, Req1),
+                    io:format("Received data: ~s~n", [UserData]),
+                    %% Send a response back to the client
+                    ResponseBody = <<"Thank you for your submission!">>,
+                    Req2 = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, ResponseBody, Req1),
                     {ok, Req2, State};
                 _ ->
                     %% Unsupported Media Type
-                    {ok, Req1} = cowboy_req:reply(415, #{<<"content-type">> => <<"text/plain">>}, <<"Unsupported Media Type">>, Req0),
+                    Req1 = cowboy_req:reply(
+                        415,
+                        #{<<"content-type">> => <<"text/plain">>},
+                        <<"Unsupported Media Type">>,
+                        Req0
+                    ),
                     {ok, Req1, State}
             end;
         _ ->
+            %% Handle other HTTP methods if necessary
             {ok, Req0, State}
     end.
