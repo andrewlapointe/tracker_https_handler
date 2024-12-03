55adc53abbae93424365b38c1bad38a8e103d8cc
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-12-03 13:51:55 -0700

    test form post method

--- src/register_form.erl
+++ src/register_form.erl
@@ -6,10 +6,10 @@ init(Req0, State) ->
     io:format("Received ~s request~n", [Method]),
     case Method of
         <<"POST">> ->
-
+            
             %% Read the request body
             case cowboy_req:read_body(Req0) of
-                {ok, _Body, Req1} ->
+                {ok, Body, Req1} ->
                     % %% Parse the URL-encoded form data using http_uri:parse_query/1
                     % Fields = http_uri:parse_query(binary_to_list(Body)),
                     % %% Extract 'tracking-number' from the form fields
@@ -17,18 +17,27 @@ init(Req0, State) ->
                     % io:format("Received tracking number: ~s~n", [TrackingNumber]),
                     % %% Send a response back to the client
                     % QueryString = cowboy_req:qs(Req1),
-                    ParsedQs = cowboy_req:qs(Req1),
-                    % AtomsQs = [{binary_to_existing_atom(K, latin1), V}
-                    %     || {K, V} <- ParsedQs],
-                    Headers = #{<<"content-type">> => <<"text/html">>},
-                    ResponseBody = io_lib:format("<html><body><h1>Tracking Number Received: ~p</h1></body></html>", [ParsedQs]),
-                    Req2 = cowboy_req:reply(
-                        200,
-                        Headers,
-                        ResponseBody,
-                        Req1
-                    ),
-                    {ok, Req2, State};
+
+                    % ParsedQs = cowboy_req:parse_qs(Req1),
+
+                    case jsx:decode(Body, [returnmaps]) of
+                        {error, _} ->
+                            %% Invalid JSON
+                            {ok, Resp} = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>},
+                                                        <<"Invalid JSON payload">>, Req1),
+                            {halt, Resp, State};
+                        #{<<"tracking-number">> := <<1>>} ->
+                            %% Query package information
+                            Headers = #{<<"content-type">> => <<"text/html">>},
+                            ResponseBody = io_lib:format("<html><body><h1>Tracking Number Received: ~p</h1></body></html>", ["Test JSX"]),
+                            Req2 = cowboy_req:reply(
+                                200,
+                                Headers,
+                                ResponseBody,
+                                Req1
+                            ),
+                            {halt, Req2, State}
+                    end;
                 {more, _, _} ->
                     %% Handle cases where the body is too large or needs to be read in chunks
                     Req1 = cowboy_req:reply(
