2c236eaecfe9e8adb441ce4f179cb97a06f60c08
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-12-03 13:06:23 -0700

    test form post method

--- src/register_form.erl
+++ src/register_form.erl
@@ -8,45 +8,42 @@ init(Req0, State) ->
         <<"POST">> ->
             %% Read the request body
             case cowboy_req:read_body(Req0) of
-            %     {ok, _Body, _Req1} ->
-            %         % %% Parse the URL-encoded form data using http_uri:parse_query/1
-            %         % Fields = http_uri:parse_query(binary_to_list(Body)),
-            %         % %% Extract 'tracking-number' from the form fields
-            %         % TrackingNumber = proplists:get_value("tracking-number", Fields, ""),
-            %         % io:format("Received tracking number: ~s~n", [TrackingNumber]),
-            %         % %% Send a response back to the client
-            %         % ResponseBody = io_lib:format(
-            %         %     "<html><body><h1>Tracking Number Received: ~s</h1></body></html>",
-            %         %     [TrackingNumber]
-            %         % ),
-            %         % Req2 = cowboy_req:reply(
-            %         %     200,
-            %         %     [{<<"content-type">>, <<"text/html">>}],
-            %         %     ResponseBody,
-            %         %     Req1
-            %         % ),
-            %         % {ok, Req2, State};
-            %         ok;
-            %     {more, _, _} ->
-            %         %% Handle cases where the body is too large or needs to be read in chunks
-            %         Req1 = cowboy_req:reply(
-            %             413,
-            %             [{<<"content-type">>, <<"text/plain">>}],
-            %             <<"Payload Too Large">>,
-            %             Req0
-            %         ),
-            %         {ok, Req1, State};
-            %     {error, Reason} ->
-            %         io:format("Error reading body: ~p~n", [Reason]),
-            %         Req1 = cowboy_req:reply(
-            %             400,
-            %             [{<<"content-type">>, <<"text/plain">>}],
-            %             <<"Bad Request">>,
-            %             Req0
-            %         ),
-            %         {ok, Req1, State};
-                _ ->
-                    other
+                {ok, _Body, Req1} ->
+                    % %% Parse the URL-encoded form data using http_uri:parse_query/1
+                    % Fields = http_uri:parse_query(binary_to_list(Body)),
+                    % %% Extract 'tracking-number' from the form fields
+                    % TrackingNumber = proplists:get_value("tracking-number", Fields, ""),
+                    % io:format("Received tracking number: ~s~n", [TrackingNumber]),
+                    % %% Send a response back to the client
+                    ResponseBody = io_lib:format(
+                        "<html><body><h1>Tracking Number Received: Test Val</h1></body></html>"
+                        % [TrackingNumber]
+                    ),
+                    Req2 = cowboy_req:reply(
+                        200,
+                        [{<<"content-type">>, <<"text/html">>}],
+                        ResponseBody,
+                        Req1
+                    ),
+                    {ok, Req2, State};
+                {more, _, _} ->
+                    %% Handle cases where the body is too large or needs to be read in chunks
+                    Req1 = cowboy_req:reply(
+                        413,
+                        [{<<"content-type">>, <<"text/plain">>}],
+                        <<"Payload Too Large">>,
+                        Req0
+                    ),
+                    {ok, Req1, State};
+                {error, Reason} ->
+                    io:format("Error reading body: ~p~n", [Reason]),
+                    Req1 = cowboy_req:reply(
+                        400,
+                        [{<<"content-type">>, <<"text/plain">>}],
+                        <<"Bad Request">>,
+                        Req0
+                    ),
+                    {ok, Req1, State}
             end;
         _ ->
             %% For methods other than POST
