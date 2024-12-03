4bb0ce470a1f5889b315e463c2c6409f84d98070
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-12-03 11:54:13 -0700

    test form post method

--- src/register_form.erl
+++ src/register_form.erl
@@ -1,32 +1,42 @@
 -module(register_form).
 -export([init/2]).
-% -include_lib("cowboy/include/cowboy.hrl").
-
 
 init(Req0, State) ->
     Method = cowboy_req:method(Req0),
-    io:format(Req0),
+    io:format("Received request: ~p~n", [Req0]),
     io:format("Received ~s request~n", [Method]),
     case Method of
         <<"POST">> ->
             %% Read and parse the URL-encoded form data
-            {ok, Fields, Req1} = cowboy_req:body_qs(Req0),
-            %% Extract 'tracking-number' from the form fields
-            TrackingNumber = proplists:get_value(<<"tracking-number">>, Fields, <<"">>),
-            %% Process the data
-            io:format("Received tracking number: ~s~n", [TrackingNumber]),
-            %% Send a response back to the client
-            ResponseBody = io_lib:format(
-                "<html><body><h1>Tracking Number Received: ~s</h1></body></html>",
-                [TrackingNumber]
-            ),
-            Req2 = cowboy_req:reply(
-                200,
-                #{<<"content-type">> => <<"text/html">>},
-                ResponseBody,
-                Req1
-            ),
-            {ok, Req2, State};
+            case cowboy_req:body_qs(Req0) of
+                {ok, Fields, Req1} ->
+                    %% Extract 'tracking-number' from the form fields
+                    TrackingNumber = proplists:get_value(<<"tracking-number">>, Fields, <<"">>),
+                    io:format("Received tracking number: ~s~n", [TrackingNumber]),
+                    %% Send a response back to the client
+                    ResponseBody = iolist_to_binary(
+                        io_lib:format(
+                            "<html><body><h1>Tracking Number Received: ~s</h1></body></html>",
+                            [TrackingNumber]
+                        )
+                    ),
+                    Req2 = cowboy_req:reply(
+                        200,
+                        #{<<"content-type">> => <<"text/html">>},
+                        ResponseBody,
+                        Req1
+                    ),
+                    {ok, Req2, State};
+                {error, Reason} ->
+                    io:format("Error parsing form data: ~p~n", [Reason]),
+                    Req1 = cowboy_req:reply(
+                        400,
+                        #{<<"content-type">> => <<"text/plain">>},
+                        <<"Bad Request">>,
+                        Req0
+                    ),
+                    {ok, Req1, State}
+            end;
         _ ->
             %% For methods other than POST
             Req1 = cowboy_req:reply(
