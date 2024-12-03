8e9e49b2a66635df1e7069e43696c0bf5b2067e9
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-12-03 13:54:58 -0700

    test form post method

--- src/register_form.erl
+++ src/register_form.erl
@@ -18,26 +18,19 @@ init(Req0, State) ->
                     % %% Send a response back to the client
                     % QueryString = cowboy_req:qs(Req1),
 
-                    % ParsedQs = cowboy_req:parse_qs(Req1),
+                    Decode = jsx:decode(Body)
 
-                    case jsx:decode(Body, [returnmaps]) of
-                        {error, _} ->
-                            %% Invalid JSON
-                            {ok, Resp} = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>},
-                                                        <<"Invalid JSON payload">>, Req1),
-                            {halt, Resp, State};
-                        _ ->
-                            %% Query package information
-                            Headers = #{<<"content-type">> => <<"text/html">>},
-                            ResponseBody = io_lib:format("<html><body><h1>Tracking Number Received: ~p</h1></body></html>", ["Test JSX"]),
-                            Req2 = cowboy_req:reply(
-                                200,
-                                Headers,
-                                ResponseBody,
-                                Req1
-                            ),
-                            {halt, Req2, State}
-                    end;
+                    % AtomsQs = [{binary_to_existing_atom(K, latin1), V}
+                    %     || {K, V} <- ParsedQs],
+                    Headers = #{<<"content-type">> => <<"text/html">>},
+                    ResponseBody = io_lib:format("<html><body><h1>Tracking Number Received: ~p</h1></body></html>", [Decode]),
+                    Req2 = cowboy_req:reply(
+                        200,
+                        Headers,
+                        ResponseBody,
+                        Req1
+                    ),
+                    {ok, Req2, State};
                 {more, _, _} ->
                     %% Handle cases where the body is too large or needs to be read in chunks
                     Req1 = cowboy_req:reply(
