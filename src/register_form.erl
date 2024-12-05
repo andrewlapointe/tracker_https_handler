699a8b3a04c4b2bb027fd75a926f2095c994b190
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-12-05 12:50:54 -0700

    read_body method

--- src/register_form.erl
+++ src/register_form.erl
@@ -6,50 +6,11 @@ init(Req0, State) ->
     io:format("Received ~s request~n", [Method]),
     case Method of
         <<"POST">> ->
-            
-            %% Read the request body
-            case cowboy_req:read_body(Req0) of
-                {ok, Body, Req1} ->
-                    % %% Parse the URL-encoded form data using http_uri:parse_query/1
-                    % Fields = http_uri:parse_query(binary_to_list(Body)),
-                    % %% Extract 'tracking-number' from the form fields
-                    % TrackingNumber = proplists:get_value("tracking-number", Fields, ""),
-                    % io:format("Received tracking number: ~s~n", [TrackingNumber]),
-                    % %% Send a response back to the client
-                    % QueryString = cowboy_req:qs(Req1),
+           Body = cowboy_req:read_body(Req0),
+            Headers = #{<<"content-type">> => <<"text/html">>},
+            Req1 = cowboy_req:reply(200, Headers, Body, Req0),
+            {ok, Req1, State};
 
-                    Decode = jsx:decode(Body)
-
-                    % AtomsQs = [{binary_to_existing_atom(K, latin1), V}
-                    %     || {K, V} <- ParsedQs],
-                    Headers = #{<<"content-type">> => <<"text/html">>},
-                    ResponseBody = io_lib:format("<html><body><h1>Tracking Number Received: ~p</h1></body></html>", [Decode]),
-                    Req2 = cowboy_req:reply(
-                        200,
-                        Headers,
-                        ResponseBody,
-                        Req1
-                    ),
-                    {ok, Req2, State};
-                {more, _, _} ->
-                    %% Handle cases where the body is too large or needs to be read in chunks
-                    Req1 = cowboy_req:reply(
-                        413,
-                        [{<<"content-type">>, <<"text/plain">>}],
-                        <<"Payload Too Large">>,
-                        Req0
-                    ),
-                    {ok, Req1, State};
-                {error, Reason} ->
-                    io:format("Error reading body: ~p~n", [Reason]),
-                    Req1 = cowboy_req:reply(
-                        400,
-                        [{<<"content-type">>, <<"text/plain">>}],
-                        <<"Bad Request">>,
-                        Req0
-                    ),
-                    {ok, Req1, State}
-            end;
         _ ->
             %% For methods other than POST
             Req1 = cowboy_req:reply(
