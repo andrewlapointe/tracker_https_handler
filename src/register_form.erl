5e49ec3b743de31afc4a97229c8a81759302b364
NoahFTW123 <noahjlarsen20@gmail.com>
2024-12-07 12:27:51 -0700

    debug stuff

--- src/register_form.erl
+++ src/register_form.erl
@@ -9,33 +9,30 @@ init(Req0, State) ->
             %% Read the binary body of the request
             case cowboy_req:read_body(Req0) of
                 {ok, BinaryData, _} ->
-                    %% Attempt to register the package
-                    try gen_server:call({registration_server, 'logic@146.190.145.34'}, {register_package, BinaryData}) of
+                    %% Call the gen_server with the binary data
+                    case gen_server:call({registration_server, 'logic@146.190.145.34'}, {register_package, BinaryData}) of
                         {ok, "Package registered", PackageKey} ->
                             io:format("Package registered with ID: ~s~n", [PackageKey]),
-                            %% Respond with success
+                            %% Respond with a success message and the package ID
                             Headers = #{<<"content-type">> => <<"text/plain">>},
-                            ReplyBody = <<"Package registered successfully! Package ID: ">> ++ PackageKey,
+                            ReplyBody = << "Package registered successfully! Package ID: ", PackageKey/binary >>,
                             Req1 = cowboy_req:reply(200, Headers, ReplyBody, Req0),
                             {ok, Req1, State};
                         {error, Reason} ->
+                            %% Handle registration errors
                             io:format("Failed to register package: ~p~n", [Reason]),
                             Headers = #{<<"content-type">> => <<"text/plain">>},
-                            ReplyBody = <<"Failed to register package. Reason: ">> ++ atom_to_binary(Reason, utf8),
+                            ReasonBinary = atom_to_binary(Reason, utf8),
+                            ReplyBody = <<"Failed to register package. Reason: ", ReasonBinary/binary>>,
                             Req1 = cowboy_req:reply(500, Headers, ReplyBody, Req0),
-                            {ok, Req1, State}
-                    catch
-                        _:Error ->
-                            io:format("Unexpected error during registration: ~p~n", [Error]),
-                            Headers = #{<<"content-type">> => <<"text/plain">>},
-                            Req1 = cowboy_req:reply(500, Headers, <<"Internal Server Error">>, Req0),
                             {ok, Req1, State}
                     end;
                 {error, Reason} ->
                     %% Handle errors in reading the body
                     io:format("Error reading body: ~p~n", [Reason]),
                     Headers = #{<<"content-type">> => <<"text/plain">>},
-                    ReplyBody = <<"Unable to process request. Reason: ">> ++ atom_to_binary(Reason, utf8),
+                    ReasonBinary = atom_to_binary(Reason, utf8),
+                    ReplyBody = <<"Unable to process request. Reason: ", ReasonBinary/binary>>,
                     Req1 = cowboy_req:reply(400, Headers, ReplyBody, Req0),
                     {ok, Req1, State}
             end;
