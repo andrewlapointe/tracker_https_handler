694d6e0fd87ce2e55e566df6c81b2e705abf2bec
Aidan Foushee <aidanfoushee@gmail.com>
2024-12-07 11:52:45 -0700

    removed binary for response

--- src/register_form.erl
+++ src/register_form.erl
@@ -21,7 +21,7 @@ init(Req0, State) ->
                         {ok, Status} ->
                             %% Respond with success, displaying the package status
                             Headers = #{<<"content-type">> => <<"text/plain">>},
-                            ResponseBody = <<"Tracking request received. Status: ">> ++ binary_to_term(Status),
+                            ResponseBody = "Tracking request received. Status: " ++ Status,
                             Req2 = cowboy_req:reply(200, Headers, ResponseBody, Req1),
                             {ok, Req2, State};
                         {error, Reason} ->
